457b52aad6b8b1979a01938655f010a0dee03221
BM <chane81@naver.com>
2022-04-05 19:42:27 +0900

    Time: 109 ms (73.16%), Space: 51 MB (92.24%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    k = nums.length - k%nums.length;
+    let arr = [...nums]
+    for(let i=0;i<nums.length;i++){
+        if(k === nums.length){
+            k=0;
+        }
+        nums[i] = arr[k]
+        k++;
+    }
+    return nums;
+};
