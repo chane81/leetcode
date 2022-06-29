ff474f2335532008589e0d9b08fab7101cecef81
BM <chane81@naver.com>
2022-06-30 01:11:55 +0900

    Time: 101 ms, Memory: 44.8 MB - LeetHub

--- max-consecutive-ones/max-consecutive-ones.js
+++ max-consecutive-ones/max-consecutive-ones.js
@@ -7,14 +7,14 @@ var findMaxConsecutiveOnes = function(nums) {
     let res = 0;
     let cnt = 0;
     
-    nums.forEach((v, idx) => {
-        if (v === 1) {
+    for(let i = 0; i < nums.length; i++) {
+        if (nums[i] === 1) {
             ++cnt;
             res = Math.max(res, cnt);
         } else {
             cnt = 0;
         }
-    });
+    };
     
     return res;
 };
