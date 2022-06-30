652760c1b4bbaccbf86cee018340c1b5f2a1178d
BM <chane81@naver.com>
2022-07-01 00:51:24 +0900

    Time: 1254 ms (5.00%), Space: 45.3 MB (5.00%) - LeetHub

---
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMoves2 = function(nums) {
+    const len = nums.length;
+    
+    const arrCalc = [];
+    let stNum;
+    
+    for(let i = 0; i < len; i++) {
+        stNum = nums[i];
+        let calc = 0;
+        
+        for (let j = 0; j < len; j++) {
+            if (stNum !== nums[j]) {
+                calc += Math.abs(stNum - nums[j]);
+            }
+        }
+        
+        arrCalc.push(calc);
+    }
+    
+    // console.log('res', stNum, arrCalc);
+    return arrCalc.sort((a, b) => a - b)[0];
+};
