1ee48b35314e1897994c455d1786e3bed870aae5
BM <chane81@naver.com>
2022-07-12 00:04:17 +0900

    Time: 138 ms (12.91%), Space: 44.9 MB (13.70%) - LeetHub

--- 202-happy-number/202-happy-number.js
+++ 202-happy-number/202-happy-number.js
@@ -3,23 +3,8 @@
  * @return {boolean}
  */
 var isHappy = function(n, hashMap = {}) {
-    // 1. input num을 toString() 변환
-    // 2. for 문으로 각 숫자를 돌면서 제곱 구해서 sum
-    // 3. sum 한 값을 재귀호출
-    // 4. 만약 sum 한 값이 1
-    
-    // 1111111
-    // 1 + 1 + 1 + 1 + 1 + 1 + 1 = 7
-    // 49
-    // 16 + 81 = 97
-    // 81 + 49 = 140
-    // 1 + 16 + 0 = 17
-    // 1 + 49 = 50
-    // 25 + 0 = 25
-    // 4 + 25 = 29
-    // 4 + 81 = 85
-    // 64 + 25 = 89
-    // 64 + 81 = 145
+    // hashMap 을 두어서 무한 순환이 되는 경우(같은 번호가 있을 경우)
+    // false 로 return 하여 끝냄
     
     if (n === 1) {
         return true;
@@ -31,6 +16,7 @@ var isHappy = function(n, hashMap = {}) {
     for (let i = 0; i < arr.length; i++) {
         sum += arr[i] * arr[i];
     };
+    
     
     if (!hashMap[sum]) {
         hashMap[sum] = sum;   
