2c82365526175d35547a5985dad8a561aa87eba8
BM <chane81@naver.com>
2022-07-31 22:01:04 +0900

    Time: 330 ms (5.01%), Space: 51.3 MB (5.31%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.js
+++ 2-add-two-numbers/2-add-two-numbers.js
@@ -12,38 +12,42 @@
  */
 var addTwoNumbers = function(l1, l2) {
     const answer = new ListNode();
+    let cur = answer;
     let n1 = l1;
     let n2 = l2;
-    let cur = answer;
+    let val = n1.val + n2.val;
     let carry = false;
     
-    let val = n1.val + n2.val;
-    if(val > 9){
+    if (val > 9) {
         carry = true;
         val = val % 10;
     }
     cur.val = val;
     
-    while(n1.next || n2.next){
+    while (n1.next || n2.next) {
         n1 = n1.next || new ListNode();
         n2 = n2.next || new ListNode();
         cur.next = new ListNode();
         cur = cur.next;
         val = n1.val + n2.val;
-        if(carry){
-            val++;
+        
+        if (carry) {
+            ++val;
             carry = false;
         }
-        if(val > 9){
+        
+         if (val > 9) {
             carry = true;
             val = val % 10;
-        }
-        cur.val = val;
+         }
+         cur.val = val;
     }
     
-    if(carry){
+    if (carry) {
         cur.next = new ListNode(1);
     }
+    
+    console.log(cur.val, answer)
     
     return answer;
 };
