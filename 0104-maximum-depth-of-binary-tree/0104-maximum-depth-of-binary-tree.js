31589793fb4d1c0579ca00ba5d1caa7491848739
BM <chane81@naver.com>
2022-12-25 16:10:53 +0900

    Time: 59 ms (99.40%), Space: 45 MB (69.07%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,8 +11,57 @@
  * @return {number}
  */
 var maxDepth = function(root) {
+    // bfs
+    // return getDepthByBfs(root);
+
+    // dfs
+    // return getDepthByDfs(root);
+
     return getDepth(root);
 };
+
+/** dfs */
+const getDepthByDfs = (root) => {
+    let depth = 0;
+
+    const dfs = (root, len) => {
+        if (!root) {
+            depth = Math.max(len, depth);
+            return;
+        }
+
+        dfs(root.left, len + 1);
+        dfs(root.right, len + 1);
+    }
+
+    dfs(root, 0);
+
+    return depth;
+}
+
+/** bfs */
+const getDepthByBfs = (root) => {
+    const queue = [];
+    let depth = 0;
+
+    if (root === null || root === undefined) return 0;
+
+    queue.push(root);
+
+    while (queue.length > 0) {
+        const len = queue.length;
+
+        for (let i = 0; i < len; i++) {
+            if (queue[i].left) queue.push(queue[i].left);
+            if (queue[i].right) queue.push(queue[i].right);
+        }
+        
+        queue.splice(0, len);
+        depth++;
+    }
+
+    return depth;
+}
 
 const getDepth = (root) => {
     if (root === null || root === undefined) return 0;
