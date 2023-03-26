31cf045ddb0db1d0fd46dcf1e576b2be42dc4e17
BM <chane81@naver.com>
2023-03-26 18:53:35 +0900

    Update 0070-climbing-stairs.js

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -13,19 +13,34 @@ var climbStairs = function(n) {
 
     // console.log('n', n, arr[n]);
 
-    if (n === 1) return 1;
-    if (n === 2) return 2;
+    // if (n === 1) return 1;
+    // if (n === 2) return 2;
 
-    if (arr[n] !== undefined) {
-        return arr[n];
+    // if (arr[n] !== undefined) {
+    //     return arr[n];
+    // }
+
+    // let p1 = climbStairs(n - 1);
+    // let p2 = climbStairs(n - 2);
+
+    // arr[n] = p1 + p2;
+
+    // return p1 + p2;
+    
+    
+    
+    
+    const a = [];
+    a[1] = 1;
+    a[2] = 2;
+    a[3] = 3;
+    
+    // answer == k(n - 1) + k(n - 2)
+    
+    let answer = 0;
+    for(let i = 3; i <= n; i++) {
+        a[i] = a[i - 1] + a[i - 2];
     }
-
-    let p1 = climbStairs(n - 1);
-    let p2 = climbStairs(n - 2);
-
-    arr[n] = p1 + p2;
-
-    return p1 + p2;
+    
+    return a[n];
 };
-
-let arr = new Array();
